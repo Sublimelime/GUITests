0952131d4c8f0a2086ae9aaf49d19f912a81d9ee
NoahNMorton <noahmorton98@gmail.com>
2016-12-09 14:20:15 -0600

    Added labels to screen

--- FinalCalculator/src/FinalCalcPanel.java
+++ FinalCalculator/src/FinalCalcPanel.java
@@ -12,9 +12,30 @@ public class FinalCalcPanel extends JPanel implements Runnable {
 
     public FinalCalcPanel() {
         setLayout(null);
-        setSize(320, 350);
+        setSize(350, 550);
         Thread t = new Thread(this);
         t.start();
+
+        JLabel[] labels = new JLabel[9];
+        JTextField[] textFields = new JTextField[8];
+        //stuff for the combo box
+        String[] choices = new String[6];
+        for (int i = 1; i < choices.length; i++) { //init list of choices
+            choices[i] = "" + i;
+        }
+        JComboBox<String> dropDown = new JComboBox<>(choices);
+
+        //LABELS CODE ------------------
+        String[] labelStrings = {"Total term weight: ", "Final Weight: ", "Number of terms: ", "Grade wanted: ", "Term 1 grade: ",
+                "Term 2 grade: ", "Term 3 grade: ", "Term 4 grade: ", "Term 5 grade: "};
+        for (int i = 0; i < labels.length; i++) { //init and set the label text for all labels
+            labels[i] = new JLabel(labelStrings[i]);
+            labels[i].setBounds(10, (i * 50) + 20, 150, 40);
+            labels[i].setEnabled(true);
+            add(labels[i]);
+        }
+
+
     }
 
     @Override
