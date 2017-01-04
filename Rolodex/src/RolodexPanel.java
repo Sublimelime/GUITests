54f0107ac3a3734cbd100b8cffa51f61c34cd9f1
NoahNMorton <noahmorton98@gmail.com>
2017-01-04 14:31:36 -0600

    Added labels and toString to entry.

--- Rolodex/src/RolodexPanel.java
+++ Rolodex/src/RolodexPanel.java
@@ -11,14 +11,24 @@ import java.util.ArrayList;
 
 public class RolodexPanel extends JPanel {
 
+    private final JLabel[] labels = new JLabel[4];
+    private final JTextField[] textFields = new JTextField[4];
     ArrayList<Entry> entries = new ArrayList<>();
     private JList list = new JList();
-
 
 
     public RolodexPanel() {
         setLayout(null);
         setSize(650, 650);
+
+        for (int i = 0; i < textFields.length; i++) { //todo set bounds for both of these
+            textFields[i] = new JTextField();
+        }
+
+        labels[0] = new JLabel("First Name: ");
+        labels[1] = new JLabel("Last Name: ");
+        labels[2] = new JLabel("Address: ");
+        labels[3] = new JLabel("Phone #: ");
 
         list.setEnabled(true);
         list.setBounds(0, 0, getWidth() / 3, getHeight());
